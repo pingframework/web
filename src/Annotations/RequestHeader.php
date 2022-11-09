97c80be9896000729676e18aed8238bc5cd415fa
oleg <oleg@bbumgames.com>
2022-11-09 17:03:59 +0200

    Fixed license doc header

--- src/Annotations/RequestHeader.php
+++ src/Annotations/RequestHeader.php
@@ -3,19 +3,31 @@
 /**
  * Ping Web
  *
- * NOTICE OF LICENSE
+ * MIT License
+ * 
+ * Copyright (c) 2022 pingframework
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  *
- * This source file is subject to the Open Software License (OSL 3.0)
- * that is bundled with this package in the file LICENSE.txt.
- * It is also available through the world-wide-web at this URL:
- * Json RPC://opensource.org/licenses/osl-3.0.php
- * If you did not receive a copy of the license and are unable to
- * obtain it through the world-wide-web, please send an email
- * to license@phpsuit.net so we can send you a copy immediately.
- *
- * @author    Oleg Bronzov <oleg@bbumgames.com>
+ * @author    Oleg Bronzov <oleg.bronzov@gmail.com>
  * @copyright 2022
- * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @license   https://opensource.org/licenses/MIT  The MIT License
  */
 
 declare(strict_types=1);
@@ -34,9 +46,9 @@ use ReflectionParameter;
 use ReflectionProperty;
 
 /**
- * @author    Oleg Bronzov <oleg@bbumgames.com>
+ * @author    Oleg Bronzov <oleg.bronzov@gmail.com>
  * @copyright 2022
- * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @license   https://opensource.org/licenses/MIT  The MIT License
  */
 #[Attribute(Attribute::TARGET_PARAMETER)]
 class RequestHeader implements Injector
